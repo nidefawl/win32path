ecb4955533fbf88be114e5fee5e815c41b841fc2
Michael Hept <michael.hept@gmail.com>
2022-02-13 12:02:34 +0100

    Don't rely on PATHEXT for .py

--- ENVSET.bat
+++ ENVSET.bat
@@ -3,4 +3,4 @@
 SET ENVSET_CALL=-tw -ns %1
 if "%~1"=="" ( SET ENVSET_CALL=-tw -ns CLANG)
 if "%~1"=="-v" ( echo on & SET ENVSET_CALL=-tw -ns %~2)
-FOR /F "usebackq TOKENS=1,* DELIMS==" %%G IN (`%~dp0\ENV_readini.py %ENVSET_CALL%`) DO ( set %%G=%%~H)
+FOR /F "usebackq TOKENS=1,* DELIMS==" %%G IN (`python %~dp0\ENV_readini.py %ENVSET_CALL%`) DO ( set %%G=%%~H)
